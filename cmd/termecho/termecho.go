a730714de008ea6d9de126420828c658b3b90b2f
Frank Braun <frank@cryptogroup.net>
2016-02-12 17:10:23 +0000

    add termecho command

---
+++ cmd/termecho/termecho.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2016 Frank Braun <frank@cryptogroup.net>
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+// termecho is a test program that echos lines typed in a terminal.
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func termechoMain() error {
+	oldState, err := terminal.MakeRaw(int(syscall.Stdin))
+	if err != nil {
+		return err
+	}
+	defer terminal.Restore(int(syscall.Stdin), oldState)
+	fmt.Println("enter 'q' to quit")
+	t := terminal.NewTerminal(os.Stdin, "> ")
+	for {
+		line, err := t.ReadLine()
+		if err != nil {
+			return err
+		}
+		if line == "q" {
+			break
+		}
+		fmt.Println(line)
+	}
+	return nil
+}
+
+func fatal(err error) {
+	fmt.Fprintf(os.Stderr, "%s: error: %s\n", os.Args[0], err)
+	os.Exit(1)
+}
+
+func main() {
+	// work around defer not working after os.Exit()
+	if err := termechoMain(); err != nil {
+		fatal(err)
+	}
+}
