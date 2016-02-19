af59f47623aef5754edeb9132b4b8223d1e4f6a8
Frank Braun <frank@cryptogroup.net>
2016-02-19 00:57:17 +0000

    add jsonfmt command

---
+++ cmd/jsonfmt/jsonfmt.go
@@ -0,0 +1,62 @@
+// Copyright (c) 2016 Frank Braun <frank@cryptogroup.net>
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+// jsonfmt formats JSON files in place.
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func formatJSON(filename string) error {
+	in, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	var jsn interface{}
+	if err := json.Unmarshal(in, &jsn); err != nil {
+		return err
+	}
+	out, err := json.MarshalIndent(jsn, "", "  ")
+	if err != nil {
+		return err
+	}
+	fp, err := ioutil.TempFile("", "jsonfmt")
+	if err != nil {
+		return err
+	}
+	if _, err := fmt.Fprintln(fp, string(out)); err != nil {
+		fp.Close()
+		return err
+	}
+	if err := fp.Close(); err != nil {
+		return err
+	}
+	if err := os.Rename(fp.Name(), filename); err != nil {
+		return err
+	}
+	return nil
+}
+
+func fatal(err error) {
+	fmt.Fprintf(os.Stderr, "%s: error: %s\n", os.Args[0], err)
+	os.Exit(1)
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s JSON_file\n", os.Args[0])
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		usage()
+	}
+	if err := formatJSON(os.Args[1]); err != nil {
+		fatal(err)
+	}
+}
