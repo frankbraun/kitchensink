f9e648bf1815a99371fb033d28704487ec2c9020
Frank Braun <frank@cryptogroup.net>
2017-05-25 16:36:57 +0000

    mdserve: use generic renderer

--- cmd/mdserve/mdserve.go
+++ cmd/mdserve/mdserve.go
@@ -14,6 +14,26 @@ import (
 	"github.com/russross/blackfriday"
 )
 
+const (
+	commonHtmlFlags = 0 |
+		blackfriday.HTML_USE_XHTML |
+		blackfriday.HTML_USE_SMARTYPANTS |
+		blackfriday.HTML_SMARTYPANTS_FRACTIONS |
+		blackfriday.HTML_SMARTYPANTS_DASHES |
+		blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
+
+	commonExtensions = 0 |
+		blackfriday.EXTENSION_NO_INTRA_EMPHASIS |
+		blackfriday.EXTENSION_TABLES |
+		blackfriday.EXTENSION_FENCED_CODE |
+		blackfriday.EXTENSION_AUTOLINK |
+		blackfriday.EXTENSION_STRIKETHROUGH |
+		blackfriday.EXTENSION_SPACE_HEADERS |
+		blackfriday.EXTENSION_HEADER_IDS |
+		blackfriday.EXTENSION_BACKSLASH_LINE_BREAK |
+		blackfriday.EXTENSION_DEFINITION_LISTS
+)
+
 type markdownRenderer struct{}
 
 func (r markdownRenderer) Render(filename string) ([]byte, error) {
@@ -22,7 +42,11 @@ func (r markdownRenderer) Render(filename string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	return blackfriday.MarkdownCommon(md), nil
+	renderer := blackfriday.HtmlRenderer(commonHtmlFlags, "", "")
+	options := blackfriday.Options{
+		Extensions: commonExtensions,
+	}
+	return blackfriday.MarkdownOptions(md, renderer, options), nil
 }
 
 func fatal(err error) {
