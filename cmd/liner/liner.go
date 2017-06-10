a854654ba7d25e77ab8c5572d17c2905d5836dca
Frank Braun <frank@cryptogroup.net>
2017-06-10 01:17:12 +0000

    add liner command

---
+++ cmd/liner/liner.go
@@ -0,0 +1,42 @@
+// Copyright (c) 2017 Frank Braun <frank@cryptogroup.net>
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+// liner demoes command line editor with history.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/peterh/liner"
+)
+
+func readline() error {
+	line := liner.NewLiner()
+	defer line.Close()
+	line.SetCtrlCAborts(true)
+	for {
+		ln, err := line.Prompt("> ")
+		if err != nil {
+			if err == liner.ErrPromptAborted {
+				fmt.Fprintf(os.Stderr, "aborting...\n")
+				return nil
+			}
+			return err
+		}
+		fmt.Println(ln)
+		line.AppendHistory(ln)
+	}
+}
+
+func fatal(err error) {
+	fmt.Fprintf(os.Stderr, "%s: error: %s\n", os.Args[0], err)
+	os.Exit(1)
+}
+
+func main() {
+	if err := readline(); err != nil {
+		fatal(err)
+	}
+}
