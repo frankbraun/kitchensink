0a336d6c111763d20b7b9e316ca76892dc5c0780
Frank Braun <frank@cryptogroup.net>
2017-06-12 12:43:11 +0000

    add tinfo command

---
+++ cmd/tinfo/tinfo.go
@@ -0,0 +1,42 @@
+// Copyright (c) 2017 Frank Braun <frank@cryptogroup.net>
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+// tinfo prints infos about the terminal it is running in.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/frankbraun/tcell"
+)
+
+func tinfo() error {
+	fmt.Printf("TERM=%s\n", os.Getenv("TERM"))
+	fmt.Printf("COLORTERM=%s\n", os.Getenv("COLORTERM"))
+	s, err := tcell.NewScreen()
+	if err != nil {
+		return err
+	}
+	if err = s.Init(); err != nil {
+		return err
+	}
+	w, h := s.Size()
+	colors := s.Colors()
+	s.Fini()
+	fmt.Printf("screen.Size(): width=%d, height=%d\n", w, h)
+	fmt.Printf("screen.Colors(): %d\n", colors)
+	return nil
+}
+
+func fatal(err error) {
+	fmt.Fprintf(os.Stderr, "%s: error: %s\n", os.Args[0], err)
+	os.Exit(1)
+}
+
+func main() {
+	if err := tinfo(); err != nil {
+		fatal(err)
+	}
+}
