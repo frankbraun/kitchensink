b86789cdd6e1ef2bb88ae51a859ae98984a98520
Frank Braun <frank@cryptogroup.net>
2018-05-09 19:36:02 +0000

    add btoa command

---
+++ cmd/btoa/btoa.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/ascii85"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func btoa(filename string) error {
+	src, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	dst := make([]byte, ascii85.MaxEncodedLen(len(src)))
+	n := ascii85.Encode(dst, src)
+	fmt.Println(string(dst[:n]))
+
+	return nil
+}
+
+func fatal(err error) {
+	fmt.Fprintf(os.Stderr, "%s: error: %s\n", os.Args[0], err)
+	os.Exit(1)
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s binary_file\n", os.Args[0])
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		usage()
+	}
+	if err := btoa(os.Args[1]); err != nil {
+		fatal(err)
+	}
+}
