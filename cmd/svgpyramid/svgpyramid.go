11ee058bc3be4ac656f9ec0e38c65f11a56b3c03
Frank Braun <frank@cryptogroup.net>
2018-10-25 17:13:27 +0000

    svgpyramid: switch to black

--- cmd/svgpyramid/svgpyramid.go
+++ cmd/svgpyramid/svgpyramid.go
@@ -20,7 +20,7 @@ func main() {
 
 	canvas := svg.New(os.Stdout)
 	canvas.Start(width+xOffset+xOffset, height+yOffset+yOffset)
-	s := "stroke:white;stroke-width:3"
+	s := "stroke:black;stroke-width:3"
 	canvas.Polygon([]int{
 		0 + xOffset,
 		sideLength + xOffset,
@@ -40,14 +40,14 @@ func main() {
 	canvas.Line(int(2.0*w)+xOffset, int(4.5*h)+yOffset, int(11.0*w)+xOffset, int(4.5*h)+yOffset, s)
 	canvas.Line(int(1.0*w)+xOffset, int(5.5*h)+yOffset, int(12.0*w)+xOffset, int(5.5*h)+yOffset, s)
 	canvas.Line(halfSideLength+xOffset, int(1.5*h)+yOffset, halfSideLength+xOffset, int(4.5*h)+yOffset, s)
-	canvas.Text(halfSideLength+xOffset, int(1.25*h)+yOffset, "I/F", "text-anchor:middle;font-size:25px;fill:white")
-	canvas.Text(int(5.25*w)+xOffset, int(2.75*h)+yOffset, "nyms", "text-anchor:middle;font-size:25px;fill:white")
-	canvas.Text(int(7.75*w)+xOffset, int(2.75*h)+yOffset, "DNMs", "text-anchor:middle;font-size:25px;fill:white")
-	canvas.Text(int(4.75*w)+xOffset, int(3.75*h)+yOffset, "anon.", "text-anchor:middle;font-size:25px;fill:white")
-	canvas.Text(int(4.5*w)+xOffset, int(4.25*h)+yOffset, "messaging", "text-anchor:middle;font-size:25px;fill:white")
-	canvas.Text(int((6.5+1.75)*w)+xOffset, int(3.75*h)+yOffset, "digital", "text-anchor:middle;font-size:25px;fill:white")
-	canvas.Text(int((6.5+1.75)*w)+xOffset, int(4.25*h)+yOffset, "cash", "text-anchor:middle;font-size:25px;fill:white")
-	canvas.Text(halfSideLength+xOffset, int(5.125*h)+yOffset, "secure software", "text-anchor:middle;font-size:25px;fill:white")
-	canvas.Text(halfSideLength+xOffset, int(6.125*h)+yOffset, "secure devices", "text-anchor:middle;font-size:25px;fill:white")
+	canvas.Text(halfSideLength+xOffset, int(1.25*h)+yOffset, "I/F", "text-anchor:middle;font-size:25px;fill:black")
+	canvas.Text(int(5.25*w)+xOffset, int(2.75*h)+yOffset, "nyms", "text-anchor:middle;font-size:25px;fill:black")
+	canvas.Text(int(7.75*w)+xOffset, int(2.75*h)+yOffset, "DNMs", "text-anchor:middle;font-size:25px;fill:black")
+	canvas.Text(int(4.75*w)+xOffset, int(3.75*h)+yOffset, "anon.", "text-anchor:middle;font-size:25px;fill:black")
+	canvas.Text(int(4.5*w)+xOffset, int(4.25*h)+yOffset, "messaging", "text-anchor:middle;font-size:25px;fill:black")
+	canvas.Text(int((6.5+1.75)*w)+xOffset, int(3.75*h)+yOffset, "digital", "text-anchor:middle;font-size:25px;fill:black")
+	canvas.Text(int((6.5+1.75)*w)+xOffset, int(4.25*h)+yOffset, "cash", "text-anchor:middle;font-size:25px;fill:black")
+	canvas.Text(halfSideLength+xOffset, int(5.125*h)+yOffset, "secure software", "text-anchor:middle;font-size:25px;fill:black")
+	canvas.Text(halfSideLength+xOffset, int(6.125*h)+yOffset, "secure devices", "text-anchor:middle;font-size:25px;fill:black")
 	canvas.End()
 }
